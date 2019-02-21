2041afddf0f69b467adeaeb9823d6952b21c284b
Rahul <rchawla45@gmail.com>
2019-02-21 12:51:05 -0500

    Political System Enum and Planet class created

---
+++ app/src/main/java/com/gatech/astroworld/spacetrader/entity/PoliticalSystems.java
@@ -0,0 +1,21 @@
+package com.gatech.astroworld.spacetrader.entity;
+
+public enum PoliticalSystems {
+    ANARCHY,
+    CAPITALIST_STATE,
+    COMMUNIST_STATE,
+    CONFEDERACY,
+    CORPORATE_STATE,
+    CYBERNETIC_STATE,
+    DEMOCRACY,
+    DICTATORSHIP,
+    FASCIST_STATE,
+    FEUDAL_STATE,
+    MILITARY_STATE,
+    MONARCHY,
+    PACIFIST_STATE,
+    SOCIALIST_STATE,
+    STATE_OF_SATORI,
+    TECHNOCRACY,
+    THEOCRACY;
+}
