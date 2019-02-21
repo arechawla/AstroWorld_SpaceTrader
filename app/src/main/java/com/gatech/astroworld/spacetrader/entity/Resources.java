6638809bb9d16d2e1640f5310c56b17807d7c2e7
pkhorana <pranavkhorana@gmail.com>
2019-02-21 12:37:09 -0500

    adding resources, tech level enum with solar system class

---
+++ app/src/main/java/com/gatech/astroworld/spacetrader/entity/Resources.java
@@ -0,0 +1,17 @@
+package com.gatech.astroworld.spacetrader.entity;
+
+public enum Resources {
+    NO_SPECIAL_RESOURCES,
+    MINERALRICH,
+    MINERALPOOR,
+    DESERT,
+    LOTSOFWATER,
+    RICHSOIL,
+    POORSOIL,
+    RICHFAUNA,
+    LIFELESS,
+    WEIRDMUSHROOMS,
+    LOTSOFHERBS,
+    ARTISTIC,
+    WARLIKE;
+}
---
+++ app/src/main/java/com/gatech/astroworld/spacetrader/entity/TechLevel.java
@@ -0,0 +1,14 @@
+package com.gatech.astroworld.spacetrader.entity;
+
+public enum TechLevel {
+
+    PRE_AGRICULTURE,
+    AGRICULTURE,
+    MEDIEVAL,
+    RENAISSANCE,
+    EARLY_INDUSTRIAL,
+    INDUSTRIAL,
+    POST_INDUSTRIAL,
+    HI_TECH;
+
+}
