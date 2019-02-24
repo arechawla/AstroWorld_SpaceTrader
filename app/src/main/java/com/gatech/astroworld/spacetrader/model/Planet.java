9db16632ffdcc795afb4f55d1fc5506c4cbad5d1
pkhorana <pranavkhorana@gmail.com>
2019-02-24 18:19:52 -0500

    planet changes

--- app/src/main/java/com/gatech/astroworld/spacetrader/model/Planet.java
+++ app/src/main/java/com/gatech/astroworld/spacetrader/model/Planet.java
@@ -4,9 +4,38 @@ import com.gatech.astroworld.spacetrader.entity.PoliticalSystems;
 
 public class Planet {
     private String name;
-    private int xPos;
-    private int yPos;
+    private SolarSystem.SysLocation loc;
     private PoliticalSystems gov;
+
+    public Planet(String name, SolarSystem.SysLocation loc, PoliticalSystems gov) {
+        this.name = name;
+        this.loc = loc;
+        this.gov = gov;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SolarSystem.SysLocation getLoc() {
+        return loc;
+    }
+
+    public PoliticalSystems getGov() {
+        return gov;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setLoc(SolarSystem.SysLocation location) {
+        loc = location;
+    }
+
+    public void setGov(PoliticalSystems pol) {
+        gov = pol;
+    }
 
 
 }
