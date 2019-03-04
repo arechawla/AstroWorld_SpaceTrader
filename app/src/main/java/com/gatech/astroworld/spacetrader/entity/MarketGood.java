0d3ac91b3458d8561fcaee2b299f97a02fbbe59b
Rahul <rchawla45@gmail.com>
2019-03-03 21:07:47 -0500

    Added price calculation

--- app/src/main/java/com/gatech/astroworld/spacetrader/entity/MarketGood.java
+++ app/src/main/java/com/gatech/astroworld/spacetrader/entity/MarketGood.java
@@ -1,11 +1,23 @@
 package com.gatech.astroworld.spacetrader.entity;
 
+import com.gatech.astroworld.spacetrader.model.Planet;
+import com.gatech.astroworld.spacetrader.model.SolarSystem;
+
 public class MarketGood {
     private GoodType good;
     private int count;
+    private Planet planet;
+    private SolarSystem sys;
 
-    public MarketGood() {
+    public MarketGood(GoodType good) {
         this.good = good;
         this.count = count;
     }
+
+    private int calculatePrice() {
+        int newPrice = 0;
+        newPrice = good.getBasePrice() + (good.getIPL() * sys.getTechLevel().ordinal());
+        return newPrice;
+    }
+
 }
