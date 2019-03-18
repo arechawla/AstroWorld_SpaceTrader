cf6eeb282bd71722e6fbc38d2682fd5641d16abc
Rahul <rchawla45@gmail.com>
2019-03-18 00:58:14 -0400

    Created Player Info screen and button

---
+++ app/src/main/java/com/gatech/astroworld/spacetrader/views/PlayerInfo.java
@@ -0,0 +1,16 @@
+package com.gatech.astroworld.spacetrader.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gatech.astroworld.spacetrader.R;
+
+public class PlayerInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_info);
+    }
+}
