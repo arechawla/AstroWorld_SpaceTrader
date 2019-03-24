3b9cdcd6cfd4ef1d6cb87ad262be03c1a017d0ab
Rahul <rchawla45@gmail.com>
2019-03-24 00:22:26 -0400

    added to system view activity

--- app/src/main/java/com/gatech/astroworld/spacetrader/views/systemView_Activity.java
+++ app/src/main/java/com/gatech/astroworld/spacetrader/views/systemView_Activity.java
@@ -1,14 +1,37 @@
 package com.gatech.astroworld.spacetrader.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.gatech.astroworld.spacetrader.R;
 
-public class systemView_Activity extends AppCompatActivity {
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import com.gatech.astroworld.spacetrader.R;
+import com.gatech.astroworld.spacetrader.model.Game;
+import com.gatech.astroworld.spacetrader.viewmodels.Configuration_viewmodel;
+import com.gatech.astroworld.spacetrader.viewmodels.Galaxy_viewmodel;
+import com.google.android.material.navigation.NavigationView;
+
+public class systemView_Activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RelativeLayout buttonContainer;
+    private Galaxy_viewmodel galaxyViewmodel;
+    private Configuration_viewmodel configuration_viewmodel;
+    private Game game;
+    private int viewCenterX;
+    private int viewCenterY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_view_);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
