8d53dee4207ecc5b14323224875d4af450c3ce41
pkhorana <pranavkhorana@gmail.com>
2019-04-04 23:21:21 -0400

    added value event listner

--- app/src/main/java/com/gatech/astroworld/spacetrader/model/Load.java
+++ app/src/main/java/com/gatech/astroworld/spacetrader/model/Load.java
@@ -1,7 +1,24 @@
 package com.gatech.astroworld.spacetrader.model;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+
 public class Load {
 
     //We will be reading from the dataBase
+    ValueEventListener val = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
     
 }
